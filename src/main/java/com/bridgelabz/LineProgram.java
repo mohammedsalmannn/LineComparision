d5910fce5f52c09183286b69a69d806df09f6bf6
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-21 20:55:09 +0530

    Refactor:In OOPs Concept

--- src/main/java/com/bridgelabz/LineProgram.java
+++ src/main/java/com/bridgelabz/LineProgram.java
@@ -1,11 +1,14 @@
 package com.bridgelabz;
 
+//import sun.security.util.Length;
+
 import java.util.Scanner;
+class A{
+    public  void Length()
+    {
 
-public class LineProgram {
-
-    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Values");
         Integer x1= input.nextInt();
         Integer x2= input.nextInt();
         Integer y1= input.nextInt();
@@ -19,12 +22,21 @@ public class LineProgram {
         Double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         Double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
 
+
         if (lengthOne == lengthTwo) {
             System.out.println("Equal ");
         } else {
             System.out.println("Not Equal ");
         }
-
+    }
+}
+public class LineProgram  {
+    public static void main(String[] args)
+    {
+        A lengthOne=new A();
+        A lengthTwo=new A();
+        A a1 =new A();
+        a1.Length();
 
         if(lengthOne.equals(lengthTwo)){
             System.out.println("Equal ");
@@ -32,10 +44,10 @@ public class LineProgram {
             System.out.println("Not Equal ");
         }
 
-        int diffInLength = lengthOne.compareTo(lengthTwo);
-        if (diffInLength == 0) {
+        boolean diffInLength = lengthOne.equals(lengthTwo);
+        if (diffInLength == true) {
             System.out.println("Equal of line length");
-        } else if (diffInLength > 0) {
+        } else if (diffInLength = false) {
             System.out.println("Line1 is greater in length than lengthOne ");
         } else System.out.println("Line1 is smaller in length than lengthTwo ");
     }
