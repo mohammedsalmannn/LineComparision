6d9641cdb0b2fbbd6010110ac26af2445801d644
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-21 07:58:42 +0530

    Added : Comapring two Lines

--- src/main/java/com/bridgelabz/LineProgram.java
+++ src/main/java/com/bridgelabz/LineProgram.java
@@ -16,7 +16,6 @@ public class LineProgram {
         Integer b1= input.nextInt();
         Integer b2= input.nextInt();
 
-        //Length of line calculation
         Double lengthOne = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         Double lengthTwo = Math.sqrt(Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2));
 
@@ -33,6 +32,11 @@ public class LineProgram {
             System.out.println("Not Equal ");
         }
 
-
+        int diffInLength = lengthOne.compareTo(lengthTwo);
+        if (diffInLength == 0) {
+            System.out.println("Equal of line length");
+        } else if (diffInLength > 0) {
+            System.out.println("Line1 is greater in length than lengthOne ");
+        } else System.out.println("Line1 is smaller in length than lengthTwo ");
     }
 }
